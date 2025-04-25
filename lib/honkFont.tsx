7da9afac1725a9f8d47ddd7241aeb2eece764772
Rares Sandu <rares.sandu@mindera.com>
2025-04-25 16:25:35 +0300

    small fix

--- lib/honkFont.tsx
+++ lib/honkFont.tsx
@@ -3,9 +3,10 @@ import { Ultra } from 'next/font/google'
 import { cn } from "@/lib/utils"
 
 const honk = Ultra({
-    weight: '400',})
+    weight: '400',
+    subsets: ['latin'],})
 
 // export a function that takes a string applys honk font to it
 export function honkFont(text: string) {
-    return (<span className={cn(honk.className, 'font-extrabold text-transparent bg-clip-text bg-gradient-to-b  from-hot-yellow to-hot-pink outline-text [text-shadow:_0_2px_4px_rgb(0_0_0_/_0.1)]')}>{text}</span>)
+    return (<span className={cn(honk.className, 'font-extrabold text-transparent bg-clip-text bg-gradient-to-b  from-hot-yellow to-hot-pink outline-text ')}>{text}</span>)
   }
