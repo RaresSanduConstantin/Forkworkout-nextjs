3b953f0fb8cbe7158bb3ea9dc341e5c73f4fecd1
RaresSanduConstantin <sandurares57@gmail.com>
2025-04-28 14:38:13 +0300

    add yt link

--- components/StartWorkoutComponent.tsx
+++ components/StartWorkoutComponent.tsx
@@ -11,10 +11,10 @@ import ClockImage from "@/public/clock.png";
 import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
 import Link from "next/link";
 
+import { Youtube } from "lucide-react";
+
 const restStartSound = new Audio("/sounds/rest-start.mp3");
 const restEndSound = new Audio("/sounds/rest-end.mp3");
-
-
 
 type RepStatus = "pending" | "done" | "skipped";
 
@@ -41,24 +41,23 @@ const StartWorkoutComponent = () => {
   const router = useRouter();
   const [workout, setWorkout] = useState<WorkoutSession | null>(null);
 
-
   const [resting, setResting] = useState(false);
-const [restSeconds, setRestSeconds] = useState(0);
-const [countdown, setCountdown] = useState(30);
+  const [restSeconds, setRestSeconds] = useState(0);
+  const [countdown, setCountdown] = useState(30);
 
-useEffect(() => {
+  useEffect(() => {
     if (!resting) return;
-  
+
     if (countdown === 0) {
-    restEndSound.play();
+      restEndSound.play();
       setResting(false);
       return;
     }
-  
+
     const timer = setTimeout(() => {
       setCountdown((prev) => prev - 1);
     }, 1000);
-  
+
     return () => clearTimeout(timer);
   }, [countdown, resting]);
 
@@ -69,8 +68,8 @@ useEffect(() => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const found = workouts.find((w: any) => w.id === workoutId);
       if (found) {
-        console.log('found.restasdasdasdasda', found);
-        setRestSeconds(found.rest)
+        console.log("found.restasdasdasdasda", found);
+        setRestSeconds(found.rest);
         const session: WorkoutSession = {
           ...found,
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -110,38 +109,35 @@ useEffect(() => {
     router.push("/"); // Back to home after finish
   };
 
-  
-
   if (!workout) {
     return <div className="p-6 text-center">Loading workout...</div>;
   }
 
   const handleSuccess = (exIdx: number, setIdx: number) => {
-    markSet(exIdx, setIdx, "done")
+    markSet(exIdx, setIdx, "done");
     const restValue = restSeconds;
 
-    if(!restValue) {
+    if (!restValue) {
       setRestSeconds(0);
-        setCountdown(0);
-        setResting(false);
-    return;
+      setCountdown(0);
+      setResting(false);
+      return;
     } else {
+      const seconds =
+        typeof restValue === "string" ? parseInt(restValue) : restValue;
 
-        const seconds = typeof(restValue) === "string" ? parseInt(restValue) : restValue;
-      
-        setRestSeconds(seconds);
-        setCountdown(seconds);
-        setResting(true);
+      setRestSeconds(seconds);
+      setCountdown(seconds);
+      setResting(true);
 
-        restStartSound.play(); 
-        return
-
+      restStartSound.play();
+      return;
     }
-}
+  };
 
   return (
     <div className="p-4 max-w-md mx-auto space-y-6">
-              <Button
+      <Button
         type="button"
         className=" top-5 right-5 z-50 rounded-full  shadow-lg px-5 py-3"
         variant="ghost"
@@ -156,46 +152,62 @@ useEffect(() => {
 
       {workout.exercises.map((exercise, exIdx) => (
         <div key={exIdx} className="bg-slate-100 rounded-lg p-3 space-y-2">
-          <h2 className="font-semibold text-lg break-all">{exercise.name}</h2>
-          {exercise.sets.map((set, setIdx) => {
-                    const status = set.status;
-
-                    const doneStyles = "bg-gradient-to-r from-green-400 via-green-300 to-lime-300 shadow-lg animate-popbeat";
-                    const skippedStyles = "bg-gray-300 text-gray-600 animate-pulse";
-                    const pendingStyles = "bg-white";
-                  
-                    const containerClasses = `
-                      flex items-center justify-between p-3 rounded-lg
-                      ${status === "done" ? doneStyles : status === "skipped" ? skippedStyles : pendingStyles}
-                    `;
-                  
-          
-          return (
-            <div
-              key={setIdx}
-              className={containerClasses}
+          <div className="flex items-center justify-between">
+            <h2 className="font-semibold text-lg break-all">{exercise.name}</h2>
+            <Link
+              href={`https://www.youtube.com/results?search_query=${encodeURIComponent(
+                exercise.name + " exercise #shorts"
+              )}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-red-600 hover:text-red-800"
             >
-              <Button
-                variant="outline"
-                onClick={() => markSet(exIdx, setIdx, "skipped")}
-                className="text-red-600 text-xl"
-              >
-                ❌
-              </Button>
-              <span>
-                {set.reps} reps – {set.value}
-              </span>
-              <div className="flex gap-2"></div>
+              <Youtube className="w-8 h-8" />
+            </Link>
+          </div>
+          {exercise.sets.map((set, setIdx) => {
+            const status = set.status;
 
-              <Button
-                variant="outline"
-                onClick={() => handleSuccess(exIdx, setIdx)}
-                className="text-green-600 text-xl"
-              >
-                ✅
-              </Button>
-            </div>
-          )})}
+            const doneStyles =
+              "bg-gradient-to-r from-green-400 via-green-300 to-lime-300 shadow-lg animate-popbeat";
+            const skippedStyles = "bg-gray-300 text-gray-600 animate-pulse";
+            const pendingStyles = "bg-white";
+
+            const containerClasses = `
+                      flex items-center justify-between p-3 rounded-lg
+                      ${
+                        status === "done"
+                          ? doneStyles
+                          : status === "skipped"
+                          ? skippedStyles
+                          : pendingStyles
+                      }
+                    `;
+
+            return (
+              <div key={setIdx} className={containerClasses}>
+                <Button
+                  variant="outline"
+                  onClick={() => markSet(exIdx, setIdx, "skipped")}
+                  className="text-red-600 text-xl"
+                >
+                  ❌
+                </Button>
+                <span>
+                  {set.reps} reps – {set.value}
+                </span>
+                <div className="flex gap-2"></div>
+
+                <Button
+                  variant="outline"
+                  onClick={() => handleSuccess(exIdx, setIdx)}
+                  className="text-green-600 text-xl"
+                >
+                  ✅
+                </Button>
+              </div>
+            );
+          })}
         </div>
       ))}
 
@@ -217,21 +229,18 @@ useEffect(() => {
       </div>
 
       <Dialog open={resting} onOpenChange={(open) => setResting(open)}>
-      <DialogContent className="text-center space-y-4">
+        <DialogContent className="text-center space-y-4">
+          <DialogDescription aria-describedby="rest-time" />
+          <DialogTitle aria-description="Modal" className="text-2xl font-bold">
+            {honkFont("Rest Time")}
+          </DialogTitle>
+          <Image src={ClockImage} alt="Rest Clock" className="w-28 mx-auto" />
 
-        <DialogDescription aria-describedby="rest-time" />
-        <DialogTitle aria-description="Modal" className="text-2xl font-bold">
-         {honkFont("Rest Time")}
-        </DialogTitle >
-        <Image src={ClockImage} alt="Rest Clock" className="w-28 mx-auto" />
-
-        <div className="text-4xl font-mono text-blue-600">{countdown}s</div>
-      </DialogContent>
-    </Dialog>
-
+          <div className="text-4xl font-mono text-blue-600">{countdown}s</div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
 
 export default StartWorkoutComponent;
-
